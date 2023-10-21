b9ff3dde10b1b8caeff272d9b551695bae41b5e0
Nguyen <94852638+ngonguyen1512@users.noreply.github.com>
2023-10-21 15:13:21 +0700

    21/10 completer mini cart

--- client/src/containers/Public/Navigation.js
+++ client/src/containers/Public/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { formatVietnameseToString } from '../../utils/common/formatVietnameseToString'
 import icons from '../../utils/icons'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,9 +8,10 @@ import { useLocation } from 'react-router-dom'
 import { path } from '../../utils/constant'
 import { CartContext } from '../../contexts/Cart'
 
-const { AiFillHome, BsCart4 } = icons
+const { AiFillHome, BsCart4, TiDelete } = icons
 
 const Navigation = () => {
+  const navigate = useNavigate()
   const location = useLocation()
   const dispatch = useDispatch()
   const { categories } = useSelector(state => state.app)
@@ -44,23 +45,53 @@ const Navigation = () => {
             return null
           })}
           <CartContext.Consumer>
-            {({ cartItems }) => (
-              <NavLink to={path.CART} className='content'>
-                <BsCart4 />
-                <span className=''>({cartItems.length})</span>
-              </NavLink>
-              //               <div className='relative'>
-              //                 <span className='content' onClick={() => setIsShowMiniCart(prev => !prev)}>
-              //                   <BsCart4 />
-              //                   <span className=''>({cartItems.length})</span>
-              //                 </span>
-              //                 {isShowMiniCart &&
-              //                   <div className='bg-red-500 h-16 w-44 absolute top-full z-50'>
-              //                      sdfsadfsfsf
-              //                   </div>
-              //                 }
-              //               </div>
-            )}
+            {({ cartItems, updateQuantity, removeFromCart }) => {
+              const total = cartItems.reduce((accumulator, product) =>
+                accumulator + (product.price * product.quantity), 0);
+              return (
+                // <NavLink to={path.CART} className='content'>
+                //   <BsCart4 />
+                //   <span className=''>({cartItems.length})</span>
+                // </NavLink>
+                <div className='minicart'>
+                  <span className='content' onClick={() => setIsShowMiniCart(prev => !prev)}>
+                    <BsCart4 />
+                    <span className=''>({cartItems.length})</span>
+                  </span>
+                  {isShowMiniCart &&
+                    <div className='cart-menu'>
+                      <table className='w-full'>
+                        <tr className='border-b'>
+                          <th>ID</th>
+                          <th>NAME</th>
+                          <th>QUANTITY</th>
+                          <th>PRICE</th>
+                          <th></th>
+                        </tr>
+                        {cartItems.map((product) => (
+                          <tr className='border-b border-dashed' >
+                            <td className='text-center'>{product.id}</td>
+                            <td className='pl-4'>{product.name}</td>
+                            <td className='text-center'>
+                              <button className='px-2 bg-gray-500 rounded-sm mx-3'
+                                onClick={() => updateQuantity(product, product.quantity - 1)}>-</button>
+                              {product.quantity}
+                              <button className='px-2 bg-gray-500 rounded-sm mx-3'
+                                onClick={() => updateQuantity(product, product.quantity + 1)}>+</button>
+                            </td>
+                            <td className='text-center'>{product.price.toLocaleString()}</td>
+                            <td className='text-red-500 text-xl'>
+                              <button onClick={() => removeFromCart(product.id)}><TiDelete /></button>
+                            </td>
+                          </tr>
+                        ))}
+                      </table>
+                      <p className='center' onClick={() => { navigate('/' + path.CART) }}>Go to cart</p>
+                    </div>
+                  }
+                </div>
+              )
+            }}
           </CartContext.Consumer>
         </div>
       }
